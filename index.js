aa241050a242495a7e46be1e98c55e98dbaee300
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-05-02 20:48:54 +0600

    chefsCardData added

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
+const chefsCardData = require('./data/chefsCardData.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Chef Hunter is Running')
+})
+
+app.get('/chefsCardData', (req, res)=>{
+  res.send(chefsCardData)
 })
 
 app.listen(port, () => {
